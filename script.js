e23a6102fe108ee26c4398c478b4d6607ec11ca1
Calyx404 <127593133+Calyx404@users.noreply.github.com>
2024-09-05 10:59:28 +0800

    Add load event on  click

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 $(document).ready(function () {
-    loadPageContent("article_01");
+    $("article").click(function () {
+        let target = this.getAttribute("data-target");
+        loadPageContent(target);
+    });
 });
 
 /** Loads specified HTML content into 'page-content' and sets the corresponding JavaScript file. */
 function loadPageContent(targetString) {
-    $(".right-content").load("./pages/" + targetString + ".html", function(response, status, xhr) {
+    $(".right-content").load("../pages/" + targetString + ".html", function(response, status, xhr) {
+        console.log(status);
         // Display 404 error message if the page could not be found.
         if (status === "error") { $(".right-content").html("<h1><strong>404</strong> | This page could not be found.</h1>"); }
-        else {
-            console.log("Success");
-        }
-        //     // Remove any existing script with class 'target-script'.
-        //     $("script.target-script").remove();
-
-        //     // Create a new script element to load the script file.
-        //     var script = document.createElement("script");
-        //     script.className = "target-script";
-        //     script.src = "../script/page_content_script/" + targetString + ".js";
-
-        //     // Append the new script to the head.
-        //     document.head.appendChild(script);
-        // }
     });
 }
