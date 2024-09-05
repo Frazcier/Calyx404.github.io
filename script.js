7c3456b87d9342d86f929ba3bc89b3795414897a
Calyx404 <127593133+Calyx404@users.noreply.github.com>
2024-09-05 21:47:03 +0800

    Add loading screen

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 $(document).ready(function () {
     $("article").click(function () {
-        let target = this.getAttribute("data-target");
-        loadPageContent(target);
+        loadPageContent(this.getAttribute("data-target"));
     });
+
+    triggerSearch();
 });
 
-/** Loads specified HTML content into 'page-content' and sets the corresponding JavaScript file. */
+function triggerSearch () {
+
+    $(".search-close").click(function () {
+        $(".search-close").css("visibility", "hidden");
+    });
+    
+    $(".search-input").on("input", function () {
+        if ($(".search-input").val().length > 0) {
+            $(".search-close").css("visibility", "visible");
+        } else {
+            $(".search-close").css("visibility", "hidden");
+        }
+    });
+    
+    $(".search-input").keypress(function (event) {
+        $(".search-close").css("visibility", "visible");
+        if (event.key === "Enter") {
+            event.preventDefault();
+            $(".search-icon").click();
+        }
+    });
+}
+
 function loadPageContent(targetString) {
     $(".right-content").load("../pages/" + targetString + ".html", function(response, status, xhr) {
         console.log(status);
-        // Display 404 error message if the page could not be found.
-        if (status === "error") { $(".right-content").html("<h1><strong>404</strong> | This page could not be found.</h1>"); }
+        if (status === "error") { $(".right-content").html("<h1><strong>404</strong> | This page could not be found.</h1>"); } // Display 404 error message if the page could not be found.
     });
 }
